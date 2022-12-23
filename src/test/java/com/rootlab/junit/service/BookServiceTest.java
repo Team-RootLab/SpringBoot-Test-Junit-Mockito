23550a07b9d577c1c43a0aec922f771de28e2268
Junek3 <rlarudrms28@gmail.com>
2022-12-23 14:42:56 +0900

    Mockito없이 실제 Repository로 책등록 테스트하기

--- src/test/java/com/rootlab/junit/service/BookServiceTest.java
+++ src/test/java/com/rootlab/junit/service/BookServiceTest.java
@@ -1,7 +1,40 @@
 package com.rootlab.junit.service;
 
+import com.rootlab.junit.dto.BookRequestDto;
+import com.rootlab.junit.dto.BookResponseDto;
+import com.rootlab.junit.repository.BookRepository;
+import com.rootlab.junit.util.MailSender;
+import com.rootlab.junit.util.MailSenderStub;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class BookServiceTest {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	@DisplayName("책등록하기")
+	public void registerBookTest() {
+	    // given
+		String title = "title";
+		String author = "author";
+		BookRequestDto requestDto = new BookRequestDto();
+		requestDto.setTitle(title);
+		requestDto.setAuthor(author);
+		// stub
+		MailSender mailSender = new MailSenderStub();
+	    // when
+		BookService bookService = new BookService(bookRepository, mailSender);
+		BookResponseDto responseDto = bookService.registerBook(requestDto);
+		// then
+		assertEquals(responseDto.getTitle(), requestDto.getTitle());
+		assertEquals(responseDto.getAuthor(), requestDto.getAuthor());
+	}
 
 }
