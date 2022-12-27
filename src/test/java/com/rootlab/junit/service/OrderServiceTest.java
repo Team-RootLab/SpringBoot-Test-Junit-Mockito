7bb9368e784acfa1ee46bf8dd2e545a7043e54b3
Junek3 <rlarudrms28@gmail.com>
2022-12-27 17:00:40 +0900

    OrderServiceTest에 Mockito관련 부연설명 남기기

--- src/test/java/com/rootlab/junit/service/OrderServiceTest.java
+++ src/test/java/com/rootlab/junit/service/OrderServiceTest.java
@@ -17,10 +17,16 @@ import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-// @SpringBootTest의 경우 @MockBean을 이용해 context에 Mock Bean을 load할 수 있음
-// 하지만 @SpringBookTest는 테스트 시간이 길어지게 되므로 unit test를 이용하는 것이 좋음
+// Spring Boot Container가 테스트 시에 필요하고 Bean이 Container에 존재한다면
+// @MockBean을 사용하고 그게 아닌 경우에는 @Mock을 사용한다.
+// @SpringBookTest는 테스트 시간이 길어지게 되므로 unit test를 이용하는 것이 좋음
+
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
+
+	// Test class의 필드외에도 아래와 같이 Test method의 매개변수에도 사용가능하다.
+	// void payOrderTest(@Mock OrderRepository orderRepository)
+
 	@Mock
 	private static OrderRepository orderRepository;
 	@Mock
@@ -28,14 +34,28 @@ class OrderServiceTest {
 	@InjectMocks
 	private static OrderService orderService;
 
+//	private AutoCloseable closeable;
+
 	// @ExtendWith(MockitoExtension.class) 선언시 아래와 같은 방식 없이도
 	// @Mock, @InjectMocks를 활용해 Mock객체를 주입할 수 있음
-
 //	@BeforeAll
 //	private static void setupService() {
 //		orderRepository = mock(OrderRepository.class);
 //		paymentRepository = mock(PaymentRepository.class);
 //		orderService = new OrderService(orderRepository, paymentRepository);
+//	}
+
+
+	// Mockito5에서는 @ExtendWith(MockitoExtension.class) 선언시
+	// 아래와 같은 Mock closing 작업을 자동화시킬 수 있다.
+//	@BeforeEach
+//	void initService() {
+//		closeable = MockitoAnnotations.openMocks(this);
+//	}
+//
+//	@AfterEach
+//	void closeService() throws Exception {
+//		closeable.close();
 //	}
 
 	@Test
